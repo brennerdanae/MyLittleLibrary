75a2bc6418df4970209ffa29969adce78e3e67ac
Danae Brenner <danae.brenner@westernsouthernlife.com>
2022-03-26 18:23:29 -0400

    adding a few best practice changes

--- app/src/main/java/com/example/mylittlelibrary/data/Game.kt
+++ app/src/main/java/com/example/mylittlelibrary/data/Game.kt
@@ -2,4 +2,8 @@ package com.example.mylittlelibrary.data
 
 import java.util.*
 
-data class Game(var name : String, var lendTo: String, var date: Date)
+data class Game(
+    var name: String,
+    var lendTo: String,
+    var date: Date
+)
--- app/src/main/java/com/example/mylittlelibrary/data/Movie.kt
+++ app/src/main/java/com/example/mylittlelibrary/data/Movie.kt
@@ -2,4 +2,8 @@ package com.example.mylittlelibrary.data
 
 import java.util.*
 
-data class Movie(var name : String, var lendTo: String, var date: Date)
+data class Movie(
+    var name: String,
+    var lendTo: String,
+    var date: Date
+)
