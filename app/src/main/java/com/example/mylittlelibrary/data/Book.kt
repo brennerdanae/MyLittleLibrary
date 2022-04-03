165afa29414e5a8c8c744f2190aa4a97d95c44ad
CM2inc <cepadam.11@gmail.com>
2022-04-03 17:14:51 -0400

    Made data/book into a book dto according to kotlin best practices

--- app/src/main/java/com/example/mylittlelibrary/data/Book.kt
+++ app/src/main/java/com/example/mylittlelibrary/data/Book.kt
@@ -3,12 +3,13 @@ package com.example.mylittlelibrary.data
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
 import java.util.*
 
 @Entity(tableName = "book_table")
-data class Book(
-    @PrimaryKey val id: Int,
-    @ColumnInfo(name = "name") var name: String,
-    @ColumnInfo(name = "lendTo") var lendTo: String,
-    @ColumnInfo(name = "date") var date: String
-)
+data class Book( @PrimaryKey val id: Int, @ColumnInfo(name = "name") var name: String, @ColumnInfo(name = "lendTo") var lendTo: String, @ColumnInfo(name = "date") var date: String){
+    override fun toString(): String {
+        return name
+    }
+}
+
