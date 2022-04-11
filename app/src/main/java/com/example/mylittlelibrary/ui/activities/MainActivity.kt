42f19c6d66033a1683c933dc1c157992f7701bbb
Sambaba23 <koitasamba54@gmail.com>
2022-04-10 23:00:24 -0400

    Started Work on the camera Still figuring things out

--- app/src/main/java/com/example/mylittlelibrary/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/mylittlelibrary/ui/activities/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.example.mylittlelibrary.ui.activities
 
 import android.content.ActivityNotFoundException
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.graphics.Bitmap
+import android.icu.text.SimpleDateFormat
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.core.content.FileProvider
 import com.example.mylittlelibrary.R
 import com.example.mylittlelibrary.databinding.ActivityMainBinding
+import java.io.File
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,6 +27,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var imageView: ImageView
     lateinit var button: Button
     val REQUEST_IMAGE_CAPTURE = 100
+    private var uri: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -73,5 +82,28 @@ class MainActivity : AppCompatActivity() {
         onBackPressed()
         return true
     }
+    private fun invokeCamera() {
+        val file = createImageFile()
+        try {
+            uri = FileProvider.getUriForFile(this, "app.plantdiary.fileprovider", file)
+        } catch (e: Exception) {
+            Log.e(TAG, "Error: ${e.message}")
+            var foo = e.message
+        }
+        getCameraImage.launch(uri)
+    }
+    private fun createImageFile() : File {
+        val timestamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val imageDirectory = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "Specimen_${timestamp}",
+            ".jpg",
+            imageDirectory
+        ).apply {
+            currentImagePath = absolutePath
+        }
+
+    }
+
 
 }
