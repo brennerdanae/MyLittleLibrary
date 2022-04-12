6ff0332f894cabc239b36f978d06227464859e05
Andrew Glanz <andrewvita0@gmail.com>
2022-04-12 15:00:33 -0400

    Fixed game.kt as it wasn't properly annotated. Not sure if we are using this in the project but I figured I would fix it anyway.

--- app/src/main/java/com/example/mylittlelibrary/data/Game.kt
+++ app/src/main/java/com/example/mylittlelibrary/data/Game.kt
@@ -1,10 +1,15 @@
 package com.example.mylittlelibrary.data
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 import java.util.*
 
+@Entity(tableName = "game_table")
 data class Game(
-    @SerializedName("Name") var name: String,
-    var lendTo: String,
-    var date: Date
+    @PrimaryKey val id: Int,
+    @ColumnInfo(name = "name") @SerializedName("title") var name: String,
+    @ColumnInfo(name = "lendTo") var lendTo: String,
+    @ColumnInfo(name = "date") var date: String
 )
