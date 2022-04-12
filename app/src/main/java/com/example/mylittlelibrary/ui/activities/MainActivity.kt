6cd86fd732591e0ac294be4381efc2f5b3920a8d
Andrew Glanz <andrewvita0@gmail.com>
2022-04-12 15:08:35 -0400

    Removed unfinished camera functionality. App builds w/ no errors now.

--- app/src/main/java/com/example/mylittlelibrary/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/mylittlelibrary/ui/activities/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.mylittlelibrary.ui.activities
 
 import android.content.ActivityNotFoundException
 import android.content.Intent
-import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
@@ -16,16 +15,11 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    lateinit var imageView: ImageView
-    lateinit var button: Button
-    val REQUEST_IMAGE_CAPTURE = 100
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        // testing
 
         val intent = Intent(this, ListActivity::class.java)
 
@@ -33,8 +27,6 @@ class MainActivity : AppCompatActivity() {
             intent.putExtra("Clicked", "Books")
             startActivity(intent)
         }
-
-
 
         binding.movies.setOnClickListener {
             intent.putExtra("Clicked", "Movies")
@@ -44,28 +36,6 @@ class MainActivity : AppCompatActivity() {
         binding.dvds.setOnClickListener {
             intent.putExtra("Clicked", "DVDs")
             startActivity(intent)
-
-            imageView = findViewById(R.id.imageView)
-            button = findViewById(R.id.btnAddItem)
-
-            button.setOnClickListener {
-                val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-
-                try {
-                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
-                } catch (e: ActivityNotFoundException) {
-                    // Toast.makeText(this,"Error: " + e.LocalizedMessage,Toast.LENGTH_SHORT).show()
-                    Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show()
-                }
-            }
-            fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-                if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-                    val imageBitmap = data?.extras?.get("data") as Bitmap
-                    imageView.setImageBitmap(imageBitmap)
-                } else {
-                    super.onActivityResult(requestCode, resultCode, data)
-                }
-            }
         }
 
     }
